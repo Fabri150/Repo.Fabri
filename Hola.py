dd5f5b274d66149ad29931e2c710d11db5630d1f
Fabricio Moyano <fabricx150@gmail.com>
2026-08-12 16:41:34 -0300

    s

--- Hola.py
+++ Hola.py
@@ -2,3 +2,6 @@ while nombre != "adios":
     nombre = input("A quien queres saludar (adios para salir)" )
     print(f"Hola, {nombre}!")
     lol = input("Como estas?")
+    match lol:
+        case "bien":
+            pass
